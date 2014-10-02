7e0b333d1cf1afd0b32eb1b86c829adf6fe9c759
Kristof Van Espen <kristof@creativebrain.be>
2014-10-02 16:41:23 +0200

    Small fixes
    - success feedback color fix
    - title center fix

--- public/js/script.js
+++ public/js/script.js
@@ -9,22 +9,22 @@ function callbackChimp (resp) {
     var domainErrorPart = domainError[1];
 
     if (resp.result === 'success') {
-        feedback = "Bedankt voor je inschrijving! Je ontvangt zo dadelijk een bevestigingsmail";
+        feedback = "<p class='succes'>Bedankt voor je inschrijving! Je ontvangt zo dadelijk een bevestigingsmail</p>";
     }
     else {
     	feedback = resp.msg;
 
     	switch(resp.msg) {
     		case '0 - Please enter a value':
-    			feedback = "Gelieve een e-mailadres in te vullen";
+    			feedback = "<p class='error'>Gelieve een e-mailadres in te vullen</p>";
     		break;
 
     		case '0 - An email address must contain a single @':
-                feedback = "Een e-mailadres moet een @ teken bevatten";
+                feedback = "<p class='error'>Een e-mailadres moet een @ teken bevatten</p>";
             break;
 
             case '0 - The domain portion of the email address is invalid (the portion after the @: '+ domainErrorPart +')':
-    			feedback = "Dit is een ongeldig e-mailadres";
+    			feedback = "<p class='error'>Dit is een ongeldig e-mailadres</p>";
     		break;
     	}
     }
